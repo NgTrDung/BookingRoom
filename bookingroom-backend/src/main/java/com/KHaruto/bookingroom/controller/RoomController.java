c1ca2b06d14d4b1e115037a99eb111964aa01c7d
Nguyễn Trọng Dũng <dung11112003qng@gmail.com>
2024-04-28 17:13:45 +0700

    Reapply "build: room repository, service and controller"
    This reverts commit 1a38b46305bd39389d8c36f2b9c01df060b5f4a2.

--- bookingroom-backend/src/main/java/com/KHaruto/bookingroom/controller/RoomController.java
+++ bookingroom-backend/src/main/java/com/KHaruto/bookingroom/controller/RoomController.java
@@ -1,5 +1,38 @@
 package com.KHaruto.bookingroom.controller;
 
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.sql.SQLException;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.KHaruto.bookingroom.model.Room;
+import com.KHaruto.bookingroom.response.RoomResponse;
+import com.KHaruto.bookingroom.service.RoomService;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/rooms")
 public class RoomController {
+	
+	private final RoomService roomService;
+	
+	@PostMapping("/add/new-room")
+	public ResponseEntity<RoomResponse> addNewRoom(@RequestParam("photo") MultipartFile photo,
+			@RequestParam("roomType") String roomType,
+			@RequestParam("roomPrice") BigDecimal roomPrice)throws SQLException, IOException{
+		Room savedRoom = roomService.addNewRoom(photo, roomType, roomPrice);
+		RoomResponse response = new RoomResponse(savedRoom.getRoomId(), savedRoom.getRoomType(),
+				savedRoom.getRoomPrice());
+		
+		return ResponseEntity.ok(response);
+	}
 
 }
