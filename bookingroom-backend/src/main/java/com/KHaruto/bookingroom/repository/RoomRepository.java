1a38b46305bd39389d8c36f2b9c01df060b5f4a2
Nguyễn Trọng Dũng <dung11112003qng@gmail.com>
2024-04-28 17:12:24 +0700

    Revert "build: room repository, service and controller"
    This reverts commit 3df24dcefe95e1e198e97763500b17931473316d.

--- bookingroom-backend/src/main/java/com/KHaruto/bookingroom/repository/RoomRepository.java
+++ bookingroom-backend/src/main/java/com/KHaruto/bookingroom/repository/RoomRepository.java
@@ -1,9 +1,5 @@
 package com.KHaruto.bookingroom.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.KHaruto.bookingroom.model.Room;
-
-public interface RoomRepository extends JpaRepository<Room, Long>{
+public interface RoomRepository {
 
 }
--- bookingroom-backend/src/main/java/com/KHaruto/bookingroom/service/RoomService.java
+++ bookingroom-backend/src/main/java/com/KHaruto/bookingroom/service/RoomService.java
@@ -1,17 +1,5 @@
 package com.KHaruto.bookingroom.service;
 
-import java.io.IOException;
-import java.math.BigDecimal;
-import java.sql.SQLException;
-
-import javax.sql.rowset.serial.SerialException;
-
-import org.springframework.web.multipart.MultipartFile;
-
-import com.KHaruto.bookingroom.model.Room;
-
-public interface RoomService{
-	
-	Room addNewRoom(MultipartFile photo, String roomType, BigDecimal roomPrice) throws IOException, SerialException, SQLException;
+public interface RoomService {
 
 }
